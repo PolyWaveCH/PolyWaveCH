529c98bea6054b917acf4bd43bc2a0681698a19a
PaedyG <patrick.gemperle@outlook.com>
2020-12-31 19:51:25 +0100

    i fucking hate layout in html & css

--- js/custom.js
+++ js/custom.js
@@ -5,9 +5,3 @@ $(document).ready(function () {
     $("#endlessEnemies").delay(3000).fadeIn(1500);
     $("#endlessFun").delay(4500).fadeIn(1500);
 });
-
-/*-----Script for the radial-menu animations-----*/
-$(document).ready(function() {
-    var transformValues = {1: 'translate(0px,125px)',2: 'translate(60px,105px)',3: 'translate(105px,60px)'
-        ,4: 'translate(125px,0px)',5: 'translate(35px,180px)',6: 'translate(180px,35px)'};
-});
